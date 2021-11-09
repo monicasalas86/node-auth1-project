cb7a9d34bfb1e71786f0881153db74629d608e2e
monicasalas86 <monicsalas167@gmail.com>
2021-11-08 19:43:45 -0600

    made get users endpoint with middleware, proper unauthorized error message showing

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -26,7 +26,13 @@ const Users = require('./users-model')
   }
  */
 
-
+router.get('/', restricted, (req, res, next) => {
+  Users.find()
+    .then(users => {
+      res.status(200).json(users)
+    })
+    .catch(next)
+})
 
 // Don't forget to add the router to the `exports` object so it can be required in other modules
 module.exports = router
